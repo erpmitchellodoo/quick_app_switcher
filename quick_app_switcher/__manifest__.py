5846ee34e56807c47898b8d28f8d4c020489e0da
vishnu <vishnu@gmail.com>
2026-07-06 22:05:19 +0400

    [initial]

---
+++ quick_app_switcher/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Quick App Switcher",
+    "version": "19.0.1.0.0",
+    "category": "Productivity",
+    "summary": "Switch Odoo apps instantly from compact navbar buttons",
+    "description": """
+Quick App Switcher adds compact, one-click application buttons directly to
+the Odoo backend navbar.
+
+Each user can configure their own shortcuts from Preferences. The switcher
+respects Odoo menu access rights and displays only applications available to
+the current user. It stays hidden on the Home / All Apps page.
+    """,
+    "author": "Mitchel Admin",
+    "maintainer": "Mitchel Admin",
+    "support": "erpmitchellodoo@gmail.com",
+    "depends": ["web_enterprise"],
+    "data": ["views/res_users_views.xml"],
+    "assets": {
+        "web.assets_backend": [
+            "quick_app_switcher/static/src/js/quick_app_switcher.js",
+            "quick_app_switcher/static/src/xml/quick_app_switcher.xml",
+            "quick_app_switcher/static/src/scss/quick_app_switcher.scss",
+        ],
+    },
+    "license": "LGPL-3",
+    "images": ["static/description/banner.png"],
+    "installable": True,
+    "auto_install": False,
+    "application": False,
+}
---
+++ quick_app_switcher/models/res_users.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models
+
+
+class ResUsers(models.Model):
+    _inherit = "res.users"
+
+    quick_app_menu_ids = fields.Many2many(
+        comodel_name="ir.ui.menu",
+        relation="res_users_quick_app_menu_rel",
+        column1="user_id",
+        column2="menu_id",
+        string="Quick App Shortcuts",
+        domain=[("parent_id", "=", False)],
+        help="Applications displayed in the quick app switcher in the backend header.",
+    )
+
+    @property
+    def SELF_READABLE_FIELDS(self):
+        return super().SELF_READABLE_FIELDS + ["quick_app_menu_ids"]
+
+    @property
+    def SELF_WRITEABLE_FIELDS(self):
+        return super().SELF_WRITEABLE_FIELDS + ["quick_app_menu_ids"]
+
+    @api.model
+    def get_quick_app_menu_ids(self):
+        visible_menus = self.env.user.quick_app_menu_ids._filter_visible_menus()
+
+        return visible_menus.filtered(lambda menu: not menu.parent_id).sorted(
+            key=lambda menu: (menu.sequence, menu.id)
+        ).ids
